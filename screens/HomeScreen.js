4c6fe9e55bc15e0f23f481fc6b0f5ce843093c36
Adrian Lesniak <adrian.lesniak@natwest.com>
2023-05-14 12:35:31 +0100

    Add Tailwind css

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,8 @@ import {Text, View} from 'react-native';
 
 const HomeScreen = ({}) => {
     return (
-        <View>
-            <Text>Home Screen</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-green-300">Home Screen </Text>
         </View>
     );
 };
